8d604c4381e3a41b9bd6ea9c9dcc8a2fb41647ce
Piotr Bielak <piotrbielak2@gmail.com>
2019-02-16 16:44:33 +0100

    Change "command" from optional arg to positional

--- src/args.rs
+++ src/args.rs
@@ -6,6 +6,6 @@ pub struct CliArgs {
     #[structopt(long = "docker-address", default_value = "")]
     pub docker_address: String,
 
-    #[structopt(short, long, default_value = "/bin/bash")]
+    #[structopt(default_value = "/bin/bash")]
     pub command: String,
 }
